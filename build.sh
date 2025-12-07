dc8a4f17a79cc893c8ec4324c26f8f21b4428dd7
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-12-07 05:21:50 +0100

    Add a build script.

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function exit_on_error()
+{
+    message=$1
+    echo $message;
+
+    exit 1;
+}
+
+for name in chromium-nginx php-grpc php-zts-grpc php-grpc-nginx-unit php-nginx-unit php-grpc-roadrunner php-grpc-frankenphp
+do
+    image="alpine-${name}"
+
+    echo ""
+    echo "Building image ${image}..."
+    echo ""
+    docker build --no-cache -t lagdo/${image} ./${image} || exit_on_error "Unable to build image ${image}."
+
+    echo ""
+    echo "Publishing image ${image}..."
+    echo ""
+    docker push lagdo/${image}
+done
