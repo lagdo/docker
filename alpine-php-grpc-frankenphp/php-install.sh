e8e533f86e4ef8d695aadcb71f4f588953e44278
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-12-07 04:02:02 +0100

    Change Composer command options.

--- alpine-php-grpc-frankenphp/php-install.sh
+++ alpine-php-grpc-frankenphp/php-install.sh
@@ -4,8 +4,8 @@ if [ "${COMPOSER_RUN}" == "true" ]
 then
     if [ "${COMPOSER_RUN_USER}" == "" ]
     then
-        (cd ${COMPOSER_RUN_DIR} && composer install ${COMPOSER_RUN_OPTIONS})
+        (cd ${COMPOSER_RUN_DIR} && composer ${COMPOSER_RUN_OPTIONS})
     else
-        (cd ${COMPOSER_RUN_DIR} && su ${COMPOSER_RUN_USER} -c 'composer install ${COMPOSER_RUN_OPTIONS}')
+        (cd ${COMPOSER_RUN_DIR} && su ${COMPOSER_RUN_USER} -c 'composer ${COMPOSER_RUN_OPTIONS}')
     fi
 fi
