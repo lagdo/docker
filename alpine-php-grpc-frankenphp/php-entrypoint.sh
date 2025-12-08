42ef7d09006c80c39e284702e431ba210a07913b
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-12-08 15:10:10 +0100

    Uninstall the Pie package. Too big.

--- alpine-php-grpc-frankenphp/php-entrypoint.sh
+++ alpine-php-grpc-frankenphp/php-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -e
 
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]
+then
+    set -- frankenphp run "$@"
+fi
+
 # Install Composer packages
 /usr/local/bin/php-install.sh
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-        set -- frankenphp run "$@"
-fi
 
 exec "$@"
