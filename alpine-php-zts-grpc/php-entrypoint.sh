61bb411bdf1282a5358e3b9bfbab8c3d332995cd
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-12-09 17:30:00 +0100

    New container for PHP-FPM with the GRPC extension.

--- alpine-php-zts-grpc/php-entrypoint.sh
+++ alpine-php-zts-grpc/php-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-# Install Composer packages
-/usr/local/bin/php-install.sh
-
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- php "$@"
 fi
+
+# Install Composer packages
+/usr/local/bin/php-install.sh
 
 exec "$@"
