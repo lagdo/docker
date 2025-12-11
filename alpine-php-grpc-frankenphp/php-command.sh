8583dfc66f3211eed9bef1ca8a4340579243db6e
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-12-11 15:05:30 +0100

    Add a command to clear the cache when a container is started.

--- alpine-php-grpc-frankenphp/php-command.sh
+++ alpine-php-grpc-frankenphp/php-command.sh
@@ -11,3 +11,13 @@ then
         (cd ${COMMAND_DIR} && su ${COMMAND_USER} -c 'composer ${INSTALL_OPTIONS}')
     fi
 fi
+
+if [ "${RUN_CACHE_CLEAR}" == "true" ] && [ "${CACHE_CLEAR_COMMAND}" != "" ]
+then
+    if [ "${COMMAND_USER}" == "" ]
+    then
+        (cd ${COMMAND_DIR} && php ${CACHE_CLEAR_COMMAND})
+    else
+        (cd ${COMMAND_DIR} && su ${COMMAND_USER} -c 'php ${CACHE_CLEAR_COMMAND}')
+    fi
+fi
